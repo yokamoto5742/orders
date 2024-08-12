ce6b14d87e98a90f93e8dbd86da291bd56db152f
yokamoto5742-h <yokamoto5742@gmail.com>
2024-08-12 15:52:25 +0900

    7-8

---
+++ orders/orders_service/orders_service.py
@@ -0,0 +1,42 @@
+from orders.orders_service.exceptions import OrderNotFoundError
+
+
+class OrdersService:
+    def __init__(self, orders_repository):
+        self.orders_repository = orders_repository
+
+    def place_order(self, items):
+        return self.orders_repository.add(items)
+
+    def get_order(self, order_id):
+        order = self.orders_repository.get_order(order_id)
+        if order is not None:
+            return order
+        raise OrderNotFoundError(f"Order with id {order_id} not found")
+
+    def update_order(self, order_id, items):
+        order = self.orders_repository.get(order_id)
+        if order is None:
+            raise OrderNotFoundError(f"Order with id {order_id} not found")
+        return self.orders_repository.update(order_id, {"items": items})
+
+    def list_orders(self, **filters):
+        limit = filters.pop("limit", None)
+        return self.orders_repository.list(limit, **filters)
+
+    def pay_order(self, order_id):
+        order = self.orders_repository.get_order(order_id)
+        if order is None:
+            raise OrderNotFoundError(f"Order with id {order_id} not found")
+        order.pay()
+        schedule_id = order.schedule()
+        return self.orders_repository.update(
+            order_id, {"status": "progress", "schedule_id": schedule_id}
+        )
+
+    def cancel_order(self, order_id):
+        order = self.orders_repository.get(order_id)
+        if order is None:
+            raise OrderNotFoundError(f"Order with id {order_id} not found")
+        order.cancel()
+        return self.orders_repository.update(order_id, {"status": "cancelled"})
